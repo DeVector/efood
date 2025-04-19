3c8df1f4f181823e2314942056a9f8ade4120b59
DeVector <diniz.vito@gmail.com>
2025-04-19 17:51:31 -0300

    Adicinando um limitador de texto

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -8,11 +8,17 @@ type Props = {
 }
 
 const Shop = ({ nome, foto, descricao, onClick }: Props) => {
+    const getDescricao = (descricao: string) => {
+        if (descricao.length > 95) {
+            return descricao.slice(0, 97) + '...'
+        }
+        return descricao
+    }
     return(
         <S.Card>
             <img className='imgWidth' src={foto} alt="Imagem do prato" />
             <S.Title>{nome}</S.Title>
-            <S.Describe>{descricao}</S.Describe>
+            <S.Describe>{getDescricao(descricao)}</S.Describe>
             <S.Button onClick={onClick}>Adicionar ao carrinho</S.Button>
         </S.Card>
     )
